94d92ff9005e6cfe1eb6255ebbc24b8666a0249a
Libertas <horizonvei@gmail.com>
2015-08-29 18:29:54 +0800

    add delay.c

---
+++ delay.c
@@ -0,0 +1,35 @@
+#include <at89x52.h>
+#define FREQ 11529  // kHz
+
+unsigned long hundredSeconds = 0;
+
+void int0() __interrupt(1)
+{
+    hundredSeconds++;
+}
+
+void delay(unsigned long ms)
+{
+    unsigned long lastTime;
+    lastTime = hundredSeconds;
+    while((hundredSeconds-lastTime) < ms*10);
+}
+
+int main()
+{
+    TMOD |= 0x02;  // Time0 is running at mod 3
+    TH0 = 256-FREQ/120;
+    TL0 = 256-FREQ/120;
+    TR0 = 1;
+    ET0 = 1;
+    EA = 1;
+
+    P1_0 = 0;
+    while(1)
+    {
+        delay(1000);
+        P1_0 = ! P1_0;
+    }
+    return 0;
+}
+
