c9bf7ea7cc9f978fc613c12712df8c602b993da4
Libertas <horizonvei@gmail.com>
2015-09-04 20:33:56 +0800

    add serial.c

---
+++ serial.c
@@ -0,0 +1,41 @@
+#include <8051.h>
+
+unsigned char buff;
+
+void serial(void) __interrupt(SI0_VECTOR)
+{
+    if(RI == 1)
+    {
+        RI = 0;
+        buff = SBUF;
+        switch(buff)
+        {
+            case '0':
+                P1_0 = 0;
+                break;
+            case '1':
+                P1_0 = 1;
+                break;
+        }
+    }
+    else
+    {
+        TI = 0;
+    }
+}
+
+int main()
+{
+    SCON = 0x50;
+    TMOD = 0x20;
+    TH1 = 0xfd;
+    TL1 = 0xfd;
+    TR1 = 1;
+    ES = 1;
+    EA = 1;
+    while(1)
+    {
+    }
+    return 0;
+}
+
