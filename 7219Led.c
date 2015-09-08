2426ba46f1706b87f1e4d0882e8f01a4a3925d68
Libertas <horizonvei@gmail.com>
2015-09-08 23:07:41 +0800

    add 7219

---
+++ 7219Led.c
@@ -0,0 +1,79 @@
+#include <8051.h>
+#define _nop_()	__asm NOP __endasm
+
+void sendbyte(unsigned char dat)
+{
+    unsigned char i,temp;
+    _nop_();
+    for (i=0;i<8;i++)
+   {
+       temp=dat&0x80;
+       dat=dat<<1;
+       if(temp)
+           P1_3=1;
+       else
+           P1_3=0;
+       P1_4=0;
+       _nop_();
+       P1_4=1;         
+   }
+}
+
+void sendByte(unsigned char byte)
+{
+    unsigned char i, tmp;
+    _nop_();
+    for(i=0; i<8; i++)
+    {
+        tmp = byte & 0x80;
+        byte = byte << 1;
+        if(tmp)
+            P1_3 = 1;
+        else
+            P1_3 = 0;
+
+        P1_4 = 0;
+        _nop_();
+        _nop_();
+        _nop_();
+        _nop_();
+        P1_4 = 1;
+    }
+}
+
+void sendData(unsigned int data)
+{
+    unsigned char high, low;
+    high = data >> 8;
+    low = data;
+    P1_2 = 0;
+    sendByte(high);
+    sendByte(low);
+    P1_2 = 1;
+}
+
+int main()
+{
+    P1 = 1;
+    P2 = 0;
+
+    sendData(0x0c01);
+    sendData(0x0a09);
+    sendData(0x0b03);
+    sendData(0x0900);
+    sendData(0x0f00);
+
+    while(1)
+    {
+        sendData(0x0111);
+        sendData(0x02ff);
+        sendData(0x03ee);
+        sendData(0x04dd);
+        sendData(0x05ff);
+        sendData(0x06aa);
+        sendData(0x07ee);
+        sendData(0x0888);
+    }
+    return 0;
+}
+
