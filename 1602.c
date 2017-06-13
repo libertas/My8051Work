7be9f983563b2c67ed7882264ad6feb6cf4c4ef2
libertas <horizonvei@gmail.com>
2017-06-14 00:33:51 +0800

    complete 1602

--- 1602.c
+++ 1602.c
@@ -1,15 +1,19 @@
 #include <8051.h>
 
+#define _nop_()	__asm NOP __endasm
+
 #define V0 P1_0
 #define RS P1_1
 #define RW P1_2
 #define EN P1_3
-#define DATA P0
+#define DATA P3
 
 volatile void delay()
 {
   int i;
-  for(i = 0; i < 10; i++);
+  for(i = 0; i < 100; i++) {
+    _nop_();
+  }
 }
 
 void write(unsigned char c)
@@ -37,17 +41,48 @@ void write_data(unsigned char data)
 
 void init()
 {
+  int i, j;
+
+  for(i = 0; i < 1000; i++) {
+    delay();
+  }
+
   V0 = 0;
+
+  write_com(0x38);
+
+  write_com(0x0f);
+
+  write_com(0x06);
+  write_com(0x01);
 }
 
 int main()
 {
 
+    unsigned char num = 0;
+    unsigned char table[]="Test 1";
+    unsigned char table1[]="Test 2";
 
     init();
 
-    while(1)
-    {
+    while(1){
+      write_com(0x80);
+
+      for(num=0;num<6;num++)
+      {
+          write_data(table[num]);
+          delay();
+      }
+
+      write_com(0x80+0x40);
+
+      for(num=0;num<6;num++)
+      {
+          write_data(table1[num]);
+          delay();
+      }
     }
+
     return 0;
 }
